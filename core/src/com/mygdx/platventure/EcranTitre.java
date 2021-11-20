5ea4d0089048b9d35e2fd7d30c172d420cfd8b5b
iopeti2u <hugo.iopeti5@etu.univ-lorraine.fr>
2021-11-20 10:38:00 +0100

    Musique d'introduction + récupération des données du monde.

--- core/src/com/mygdx/platventure/EcranTitre.java
+++ core/src/com/mygdx/platventure/EcranTitre.java
@@ -1,6 +1,8 @@
 package com.mygdx.platventure;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.Timer;
@@ -11,6 +13,8 @@ public class EcranTitre extends ScreenAdapter {
 
     public EcranTitre(PlatVenture platVenture) {
         this.platVenture = platVenture;
+        Music musiqueIntro = Gdx.audio.newMusic(Gdx.files.internal("sounds/win.ogg"));
+        musiqueIntro.play();
     }
 
     @Override
--- core/src/com/mygdx/platventure/Niveau.java
+++ core/src/com/mygdx/platventure/Niveau.java
@@ -4,15 +4,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 
 public class Niveau {
-    private int largeur;
-    private int hauteur;
-    private int temps;
+    private final int largeur;
+    private final int hauteur;
+    private final int temps;
     private String imageDeFond;
+    private final char[][] tabNiveau;
 
     public Niveau(String nomDeNiveau) {
         FileHandle file = Gdx.files.internal(nomDeNiveau);
-        String text = file.readString();
 
-        System.out.println(text);
+        String[] data = file.readString().split("\n"); //On split par ligne.
+        String[] text = data[0].split(" "); //Tableau qui récupère la largeur la hauteur et le temps. (On split la première ligne avec l'espace)
+
+        this.largeur = Integer.parseInt(text[0]);
+        this.hauteur = Integer.parseInt(text[1]);
+        this.temps = Integer.parseInt(text[2]);
+
+        tabNiveau = new char[largeur][hauteur];
+        for(int i = 1; i<hauteur+1;i++){
+            for(int j = 0; j<largeur-1; j++){
+                tabNiveau[j][i-1] = data[i].charAt(j);
+            }
+        }
     }
-}
+    }
